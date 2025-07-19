53fee1447aa830d3b6cdd3d6ecb352cb1a5b0262
jamesbarnes665 <jamesbarnes665@gmail.com>
2025-07-19 15:24:10 -0700

    Update LangChain import and requirements to fix deprecation warning

--- ai_engine.py
+++ ai_engine.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from config import OPENAI_API_KEY
 
